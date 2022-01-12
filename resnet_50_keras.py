4b22fb0bbf70b6d4140c03af0cd65d4ab5906c6c
Srinivas Rahul Sapireddy <32967087+srsapireddy@users.noreply.github.com>
2022-01-12 13:39:56 -0600

    Custom Activation Function Added

---
+++ resnet_50_keras.py
@@ -0,0 +1,472 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 00:50:20 2021
+
+@author: sapir
+"""
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import os
+from tensorflow import keras 
+
+dataset_path = os.listdir('rooms_dataset')
+
+room_types = os.listdir('rooms_dataset')
+#print (room_types)  #what kinds of rooms are in this dataset
+print(room_types)
+
+room_types = ['dining_room', 'living_room', 'bed_room']
+rooms_dataset = ['dining_room', 'living_room', 'bed_room']
+
+print("Types of rooms found: ", len(room_types))
+
+print("Types of rooms found: ", len(dataset_path))
+
+
+rooms = []
+
+for item in room_types:
+ # Get all the file names
+ all_rooms = os.listdir('rooms_dataset' + '/' +item)
+ #print(all_shoes)
+
+ # Add them to the list
+ for room in all_rooms:
+    rooms.append((item, str('rooms_dataset' + '/' +item) + '/' + room))
+    #print(rooms[:1])
+
+    
+# Build a dataframe        
+rooms_df = pd.DataFrame(data=rooms, columns=['room type', 'image'])
+#print(rooms_df.head())
+#print(rooms_df.tail())
+
+
+# Let's check how many samples for each category are present
+print("Total number of rooms in the dataset: ", len(rooms_df))
+
+room_count = rooms_df['room type'].value_counts()
+
+#print("rooms in each category: ")
+#print(room_count)
+
+
+import cv2
+path = 'rooms_dataset/'
+
+
+im_size = 64
+
+images = []
+labels = []
+
+for i in room_types:
+    data_path = path + str(i)  # entered in 1st folder and then 2nd folder and then 3rd folder
+    filenames = [i for i in os.listdir(data_path) ]
+   # print(filenames)  # will get the names of all images
+    for f in filenames:
+        img = cv2.imread(data_path + '/' + f)  # reading that image as array
+        #print(img)  # will get the image as an array
+        img = cv2.resize(img, (im_size, im_size))
+        images.append(img)
+        labels.append(i)
+labels    
+
+# Transform the image array to a numpy type
+images = np.array(images)
+images.shape   
+
+images = images.astype('float32') / 255.0
+
+
+from sklearn.preprocessing import LabelEncoder , OneHotEncoder
+y=rooms_df['room type'].values
+print(y[:5])
+
+# ------------------------------------------
+"""
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_x = LabelEncoder()
+X[:, 1] = labelencoder_x.fit_transform(X[:, 1])
+
+
+# remove categorical_features from here
+onehotencoder = OneHotEncoder()
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+"""
+# ------------------------------------------
+"""
+# for y
+y_labelencoder = LabelEncoder()
+y = y_labelencoder.fit_transform (y)
+print (y)
+
+y=y.reshape(-1,1)
+onehotencoder = OneHotEncoder(categorical_features=[0])  #Converted  scalar output into vector output where the correct class will be 1 and other will be 0
+Y= onehotencoder.fit_transform(y)
+Y.shape  #(393, 3)
+"""
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+y=y.reshape(-1,1)
+columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [0])],     remainder='passthrough')
+Y= columnTransformer.fit_transform(y)
+
+
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+
+images, Y = shuffle(images, Y, random_state=1)
+
+train_x, test_x, train_y, test_y = train_test_split(images, Y, test_size=0.05, random_state=415)
+
+#inpect the shape of the training and testing.
+print(train_x.shape)
+print(train_y.shape)
+print(test_x.shape)
+print(test_y.shape)
+
+import numpy as np
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+
+from keras.initializers import glorot_uniform
+import scipy.misc
+from matplotlib.pyplot import imshow
+
+from keras.initializers import glorot_uniform
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+
+# x is input, y=F(x)
+# identity block simply means input should be equal to output. 
+#  y = x + F(x)   the layers in a traditional network are learning the true output H(x)
+# F(x) = y - x   the layers in a residual network are learning the residual F(x)
+# Hence, the name: Residual Block.
+
+# Custome function
+from keras import backend as K
+from keras.utils.generic_utils import get_custom_objects
+"""
+def custom_activation(x):
+        y = (x>0) * 1.0
+        return y
+get_custom_objects().update({'custom_activation': Activation(custom_activation)})
+"""    
+def custom_relu(x):
+    return K.maximum(0.0,x)
+
+from keras.utils.generic_utils import get_custom_objects
+from keras.layers import Activation
+get_custom_objects().update({'custom_relu': Activation(custom_relu)})
+
+def identity_block(X, f, filters, stage, block):
+    """
+   
+    Arguments:
+    X -- input of shape (m, height, width, channel)
+    f -- shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Saving the input value.we need this later to add to the output. 
+    X_shortcut = X
+    
+    # First component of main path
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    # X = Activation('relu')(X)
+    X = Activation("custom_relu")(X)
+    """
+    def ReLu(x):
+    mask = (x>0) * 1.0
+    return mask *x
+
+    def ReLu(x):
+        y = (x>0) * 1.0
+    return y
+
+    ReLu(X)
+    """
+    
+    # Second component of main path (≈3 lines)
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation 
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    
+    return X
+
+
+def convolutional_block(X, f, filters, stage, block, s = 2):
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value
+    X_shortcut = X
+
+
+    # First layer 
+    X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a')(X) # 1,1 is filter size
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)  # normalization on channels
+    X = Activation('relu')(X)
+
+      
+    # Second layer  (f,f)=3*3 filter by default
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+
+    # Third layer
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+
+    ##### SHORTCUT PATH #### 
+    X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '1')(X_shortcut)
+    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value here, and pass it through a RELU activation 
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    
+    return X
+
+#Each ResNet block is either 2 layer deep
+def ResNet50(input_shape=(64, 64, 3), classes=3):
+    """
+    Implementation of the ResNet50 architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    """
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input) #3,3 padding
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1')(X) #64 filters of 7*7 
+    X = BatchNormalization(axis=3, name='bn_conv1')(X) #batchnorm applied on channels
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X) #window size is 3*3
+
+    # Stage 2
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
+    # convolutional_block is a function defined above. Convolutional_block have 3 layers.
+    #filters=[64, 64, 256] first 64 is for 1st layer and 2nd 64 is for 2nd layer and 256 is for 3rd layer of convultional block   
+    # below are the conv layers from convolutional_block function
+    #X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a')(X)
+    #X = Conv2D(F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    #X = Conv2D(F3, (1, 1), strides = (s,s), name = conv_name_base + '2a')(X)
+   
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b') 
+    #X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a')(X)
+    #X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    #X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
+  
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+    #X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a')(X)
+    #X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    #X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
+
+
+    ### START CODE HERE ###
+
+    # Stage 3 
+    X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    # Stage 4 
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage 5 
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    # AVGPOOL 
+    X = AveragePooling2D((2,2), name="avg_pool")(X)
+
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    
+    
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
+
+model = ResNet50(input_shape = (64, 64, 3), classes = 3)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+model.fit(train_x, train_y, epochs = 1, batch_size = 32) 
+
+preds = model.evaluate(test_x, test_y)
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+from matplotlib.pyplot import imread
+img_path = 'test_room.jpg'
+img = image.load_img(img_path, target_size=(64, 64))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+print('Input image shape:', x.shape)
+my_image = imread(img_path)
+imshow(my_image)
+print(model.predict(x))
+
+"""
+# working
+from keras import backend as K
+
+inp = model.input                                           # input placeholder
+outputs = [layer.output for layer in model.layers]          # all layer outputs
+functor = K.function([inp, K.learning_phase()], outputs )   # evaluation function
+"""
+
+"""
+# working
+
+input_shape = (64,64,3)
+# Testing
+test = np.random.random(input_shape)[np.newaxis,:]
+layer_outs = functor([test, int(1.)])
+print(layer_outs)
+
+
+
+print("----------------------------------------------------------------------")
+"""
+
+"""
+# input_shape=(64, 64, 3)
+# working
+from keras import backend as K
+
+# with a Sequential model
+get_3rd_layer_output = K.function([model.layers[0].input],
+                                  [model.layers[3].output])
+layer_output = get_3rd_layer_output([x])[0]
+
+print(layer_output)
+
+"""
+
+"""
+# working
+# Tensor("bn_conv1_5/cond/Identity:0", shape=(None, 32, 32, 64), dtype=float32)
+print("----------------------------------------------------------------------")
+out = model.layers[3].output
+print(out)
+"""
+
+"""
+# not working
+print("----------------------------------------------------------------------")
+#import tensorflow as tf
+inter_output_model = keras.Model(model.input, model.get_layer(index = 3).output )
+print(inter_output_model)
+"""
+
+print("----------------------------------------------------------------------")
+
+"""
+# <tensorflow.python.keras.backend.EagerExecutionFunction object at 0x000001FC003FE400>
+import tensorflow as tf
+
+
+from keras import backend as K
+
+# with a Sequential model
+get_3rd_layer_output = K.function([model.input],[model.get_layer(index=3).output])
+#layer_output = get_3rd_layer_output([x])[0]
+
+print(get_3rd_layer_output)
+"""
+
+print("----------------------------------------------------------------------")
+"""
+f = K.function([model.layers[0].input, K.learning_phase()],[model.layers[3].output])
+g = f([x])[0]
+print(g)
+print("----------------------------------------------------------------------")
+
+get_3rd_layer_output = K.function([model.layers[0].input,model.layers[2].input,K.learning_phase()],  [model.layers[3].output]) 
+layer_output = get_3rd_layer_output([x])[0]
+print(layer_output)
+"""
+
+"""
+# working
+print("----------------------------------------------------------------------")
+for layer in model.layers: 
+    print(layer.get_config(), layer.get_weights())
+"""
+
+# working
+"""
+print("----------------------------------------------------------------------")    
+weights = Conv2D.get_weights()
+print(weights)
+"""
+
+
